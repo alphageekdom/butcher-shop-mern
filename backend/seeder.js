e855a6406eeb8825570b300570907d6455702dba
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-25 11:42:15 -0800

    seed sample data

---
+++ backend/seeder.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    // Delete orders, products, and users
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // Create users in the database
+    const createdUsers = await User.insertMany(users);
+
+    // Locate the admin in database
+    const adminUser = createdUsers[0]._id;
+
+    // Create products and find admin
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    // Insert products into database
+    await Product.insertMany(sampleProducts);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    // Delete orders, products, and users
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}

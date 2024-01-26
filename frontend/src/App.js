ae4e61d4b0d4b2d2395072956a12121680341705
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-26 15:45:30 -0800

    Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -23,6 +27,7 @@ const App = () => {
           </Container>
         </main>
         <Footer />
+        <ToastContainer />
       </Router>
     </>
   );

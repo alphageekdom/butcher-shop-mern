f37b388fabf5c4107195ef12b1a9c14df6a80efe
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-26 13:28:01 -0800

    Auth Middleware and Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dontenv from 'dotenv';
 dontenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
@@ -16,6 +17,9 @@ const app = express();
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie Parser Middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
